aa8dc316a1cf47bfc0c0de3d5b35386cfe17b6cb
mdrabiulcoder <mdrabiulcoder@gmail.com>
2022-09-26 10:56:38 +0600

    added 1 month project

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+feather.replace()
+
+$(function(){
+      // counter up js start
+      $('.counter').counterUp({
+       delay: 10,
+       time: 500
+   });
+   // counter up js end
+
+})
+
+
+// venubox part start
+
+   new VenoBox({
+       selector: ".my_video"
+       
+   });
+
+// venubox part end 
